56604a12b982f299978772896b1a6a53e4dd1f26
Pete Hodgson <git@thepete.net>
2019-05-30 20:21:05 -0700

    don't require global bower (not needed any more, and not compatible with CircleCI build

--- .circleci/jobs/build.sh
+++ .circleci/jobs/build.sh
@@ -2,6 +2,4 @@
 set -e -u
 
 export BUILDSTAMP="${CIRCLE_SHA1}-${CIRCLE_BUILD_NUM}"
-
-npm install -g bower
 bundle exec rake build
--- .circleci/jobs/bundle-install.sh
+++ .circleci/jobs/bundle-install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -e -u
 
-export BUILDSTAMP="${CIRCLE_SHA1}-${CIRCLE_BUILD_NUM}"
-
 bundle install --path=vendor/bundle
